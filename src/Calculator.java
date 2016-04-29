542ebf87edf315d6a7af65e254b97fc540681bc0
nilsJavaNoob <nils1968@gmail.com>
2016-04-29 11:37:31 +0300

    first

---
+++ src/Calculator.java
@@ -0,0 +1,26 @@
+/**
+ * Created by n on 29.04.16.
+ */
+public class Calculator
+{
+    public void add()
+    {
+
+    }
+    public  void minus()
+    {
+
+    }
+    public  void div()
+    {
+
+    }
+    public  void multyply()
+    {
+
+    }
+    public void stepen()
+    {
+
+    }
+}
